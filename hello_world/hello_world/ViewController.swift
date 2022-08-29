5e5ccb9bb17e2f00a13f4e0609c5cb9a577efcda
jay <dj.min43@gmail.com>
2022-08-30 08:39:21 +0900

    hello world init

---
+++ hello_world/hello_world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello_world
+//
+//  Created by dongjoon min on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

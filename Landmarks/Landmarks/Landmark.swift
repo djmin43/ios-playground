1ae2343d76a095fb91a2694599d2822205f2819d
jay <dj.min43@gmail.com>
2022-09-04 21:18:49 +0900

    manage data

---
+++ Landmarks/Landmarks/Landmark.swift
@@ -0,0 +1,29 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by dongjoon min on 2022/09/04.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}

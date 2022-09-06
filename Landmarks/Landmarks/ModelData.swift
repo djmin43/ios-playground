af0e60a99c6ab440d38927d38d50a72bb4c696b4
Jay Min <dj.min43@gmail.com>
2022-09-07 07:05:20 +0900

    fix typo

--- Landmarks/Landmarks/ModelData.swift
+++ Landmarks/Landmarks/ModelData.swift
@@ -1,27 +1,21 @@
-//
-//  ModelData.swift
-//  Landmarks
-//
-//  Created by 민동준 on 2022/09/07.
-//
-
 import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
-    guard let file = Bundle.main.url(forResouce: filename, withExtension: nil)
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
         fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
-    do  {
+
+    do {
         data = try Data(contentsOf: file)
-        
     } catch {
-        fatalError("Couldn't load \(filenname) from main bundle:\n\(error)")
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
+
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
@@ -29,3 +23,4 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+

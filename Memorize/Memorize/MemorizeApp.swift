806dbc1c1603ac615ba55c9121f9e461d0bc88b7
jay <dj.min43@gmail.com>
2022-09-24 13:32:58 +0900

    create memorize

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by dongjoon min on 2022/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

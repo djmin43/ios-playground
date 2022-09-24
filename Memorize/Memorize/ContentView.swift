52c8f323fe3ac8c586f14d15c37512fdbc77bdf6
jay <dj.min43@gmail.com>
2022-09-24 23:11:30 +0900

    return text

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -1,15 +1,9 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by dongjoon min on 2022/09/24.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, CS193p!")
+        return Text("Hello, CS193p!")
     }
 }
 
@@ -18,3 +12,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 

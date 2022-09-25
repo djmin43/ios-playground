904de62ad09803984be4040fc7f778442df782f8
Jay Min <dj.min43@gmail.com>
2022-09-26 06:58:42 +0900

    new cardview struct

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -3,10 +3,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View{
-        ZStack {
-            RoundedRectangle(cornerRadius: 25.0)
-                .stroke(lineWidth: 3)
-            Text("Hello World!")
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(.red)
@@ -14,9 +15,25 @@ struct ContentView: View {
     }
 }
 
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                .stroke(lineWidth: 3)
+            Text("💁🏿")
+                .font(.largeTitle)
+        }
+    }
+}
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
- 
+

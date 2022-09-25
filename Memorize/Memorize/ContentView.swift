47c9b155c43dd77db516c84d80bce9c8bab59d79
jay <dj.min43@gmail.com>
2022-09-25 11:20:07 +0900

    playing with zstack

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -3,12 +3,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View{
-       return ZStack
+        return ZStack(content: {
+            RoundedRectangle(cornerRadius: 25.0)
+                .stroke(lineWidth: 3)
+            Text("Hello World!")
+        })
+        .padding(.horizontal)
+        .foregroundColor(.red)
         
-//        return RoundedRectangle(cornerRadius: 25.0)
-//            .stroke(lineWidth: 3)
-//            .padding(.horizontal)
-//            .foregroundColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
     }
 }
 

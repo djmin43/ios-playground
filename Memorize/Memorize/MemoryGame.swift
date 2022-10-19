cdf653762594e56aea2ab58e19be8b4584cff95c
Jay Min <dj.min43@gmail.com>
2022-10-20 07:14:58 +0900

    create a model

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -7,11 +7,18 @@
 
 import Foundation
 
-struct MemoryGame {
+struct MemoryGame<CardContent> {
     var cards: Array<Card>
     
-    func choose(card: Card) {
+    func choose(_ card: Card) {
         
     }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
 }
+
 

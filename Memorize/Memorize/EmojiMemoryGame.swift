e19b5f7518527e3bd32b98338b026c2309425dc0
jay <dj.min43@gmail.com>
2022-10-22 21:27:15 +0900

    mv

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 class EmojiMemoryGame {
+    private var model: MemoryGame<String> =  MemoryGame<String>(numberOfPairsOfCards: 4)
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
 
 }
 

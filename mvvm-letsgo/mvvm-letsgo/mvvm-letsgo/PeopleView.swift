64f9d7f3c95c8cc1aeb1cfa4bf27b04c6a2c908f
jay <dj.min43@gmail.com>
2022-10-23 18:36:25 +0900

    view

--- mvvm-letsgo/mvvm-letsgo/mvvm-letsgo/PeopleView.swift
+++ mvvm-letsgo/mvvm-letsgo/mvvm-letsgo/PeopleView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct PeopleView: View {
+    @ObservedObject var viewModel = PersonViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text("Hello, world!")
+                .font(.title)
+                .fontWeight(.bold)
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("555-5555")
+                Text("email@email.com")
+            }
+        }
     }
 }
 

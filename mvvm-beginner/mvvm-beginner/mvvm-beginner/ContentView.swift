6fcd0b27f9b632c5e77b11e546cdbe120c403e41
jay <dj.min43@gmail.com>
2022-10-23 22:00:43 +0900

    mvvm beginner init

---
+++ mvvm-beginner/mvvm-beginner/mvvm-beginner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mvvm-beginner
+//
+//  Created by dongjoon min on 2022/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mvvm-beginner/mvvm-beginner/mvvm-beginner/mvvm_beginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_beginnerApp.swift
+//  mvvm-beginner
+//
+//  Created by dongjoon min on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_beginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

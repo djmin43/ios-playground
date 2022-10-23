e2a4bd04855b6b97211505b7f6d36fae621276db
jay <dj.min43@gmail.com>
2022-10-23 18:21:23 +0900

    mvvm lets go init

---
+++ mvvm-letsgo/mvvm-letsgo/mvvm-letsgo/mvvm_letsgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_letsgoApp.swift
+//  mvvm-letsgo
+//
+//  Created by dongjoon min on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_letsgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

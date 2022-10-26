1b9beb68bd2a587d0e8d0a2e9cdf793971b434f6
Jay Min <dj.min43@gmail.com>
2022-10-27 06:53:48 +0900

    add states

--- LoginScreenUI/LoginScreenUI/ContentView.swift
+++ LoginScreenUI/LoginScreenUI/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
         Text("Hello, world!")
             .padding()

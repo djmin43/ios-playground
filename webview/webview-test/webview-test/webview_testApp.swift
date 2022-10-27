a60b18da75f8040bc327433eaf2fb9018f1c477e
Jay Min <dj.min43@gmail.com>
2022-10-28 07:13:01 +0900

    try webview

---
+++ webview/webview-test/webview-test/webview_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  webview_testApp.swift
+//  webview-test
+//
+//  Created by 민동준 on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct webview_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

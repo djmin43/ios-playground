4304637398a5b8e910e5c1b877ed8f7f895a0d4a
Jay Min <dj.min43@gmail.com>
2022-10-31 07:59:25 +0900

    weather init

---
+++ weatherApp/weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by 민동준 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

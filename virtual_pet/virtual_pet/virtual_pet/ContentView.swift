43ea7bd4cee315eed27d1d734f6506bc597a11ac
jay <dj.min43@gmail.com>
2022-10-30 16:30:34 +0900

    new tommy

---
+++ virtual_pet/virtual_pet/virtual_pet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  virtual_pet
+//
+//  Created by dongjoon min on 2022/10/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ virtual_pet/virtual_pet/virtual_pet/virtual_petApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtual_petApp.swift
+//  virtual_pet
+//
+//  Created by dongjoon min on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct virtual_petApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d64562b7b345a4a770dcf9d801b4cf9bcd9c988e
jay <dj.min43@gmail.com>
2022-11-01 22:19:13 +0900

    chore

---
+++ weather-app-fundamentals/weatherAppFundamental/weatherAppFundamental/weatherAppFundamentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppFundamentalApp.swift
+//  weatherAppFundamental
+//
+//  Created by dongjoon min on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppFundamentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

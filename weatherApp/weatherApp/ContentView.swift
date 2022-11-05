977a7bb77cf78cd36c923b48d40d4c0987b4518c
jay <dj.min43@gmail.com>
2022-11-06 08:40:00 +0900

    fill up colour

--- weatherApp/weatherApp/ContentView.swift
+++ weatherApp/weatherApp/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-            Text("hello world")
+        ZStack{
+            Color(.blue)
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 

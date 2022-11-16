8f9f3f45cbbff54b91e3fda6674ca98484d15120
jay <dj.min43@gmail.com>
2022-11-17 07:52:13 +0900

    moves

--- ttt/ttt/ttt/ContentView.swift
+++ ttt/ttt/ttt/ContentView.swift
@@ -7,13 +7,17 @@
 
 import SwiftUI
 
-let columns: [GridItem] = [
-    GridItem(.flexible()),
-    GridItem(.flexible()),
-    GridItem(.flexible())
-]
+
+
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
@@ -26,6 +30,11 @@ struct ContentView: View {
                                 .opacity(0.5)
                                 .frame(width: geometry.size.width/3 - 15,
                                        height: geometry.size.width/3 - 15)
+                            
+                            Image(systemName: "circle")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
                         }
                         
                     }
@@ -37,6 +46,19 @@ struct ContentView: View {
             
         }
      
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 

d48f7d5ae76234951aae58fe2512058b3ae1265d
jay <dj.min43@gmail.com>
2022-11-19 18:41:25 +0900

    add circle

--- scrumdinger/scrumdinger/scrumdinger/MeetingView.swift
+++ scrumdinger/scrumdinger/scrumdinger/MeetingView.swift
@@ -12,15 +12,22 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
-                
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
             }
         }
     }

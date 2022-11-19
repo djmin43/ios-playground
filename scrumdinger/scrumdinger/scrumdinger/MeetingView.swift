2918030063eafc35a634329b543dfd24621ad775
jay <dj.min43@gmail.com>
2022-11-19 18:32:50 +0900

    add labels

--- scrumdinger/scrumdinger/scrumdinger/MeetingView.swift
+++ scrumdinger/scrumdinger/scrumdinger/MeetingView.swift
@@ -11,6 +11,17 @@ struct MeetingView: View {
     var body: some View {
         VStack {
             ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+                
+            }
         }
     }
 }

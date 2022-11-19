338c0ab537164ca95bb97abfa1cf7ef0321923e8
jay <dj.min43@gmail.com>
2022-11-19 18:17:53 +0900

    scrumdinger init

---
+++ scrumdinger/scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by dongjoon min on 2022/11/19.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

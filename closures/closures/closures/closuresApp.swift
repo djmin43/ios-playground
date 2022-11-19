68caf2d405c533f3aa7026996d82f4691a750adc
jay <dj.min43@gmail.com>
2022-11-19 18:16:51 +0900

    chore

---
+++ closures/closures/closures/closuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  closuresApp.swift
+//  closures
+//
+//  Created by dongjoon min on 2022/11/19.
+//
+
+import SwiftUI
+
+@main
+struct closuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

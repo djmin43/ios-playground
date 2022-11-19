d698e5a3bba63a4d62391a5168b009d61d74fa9a
jay <dj.min43@gmail.com>
2022-11-19 18:24:41 +0900

    refactor name

--- scrumdinger/scrumdinger/scrumdinger/MeetingView.swift
+++ scrumdinger/scrumdinger/scrumdinger/MeetingView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MeetingView()
     }
 }
--- scrumdinger/scrumdinger/scrumdinger/scrumdingerApp.swift
+++ scrumdinger/scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct scrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }

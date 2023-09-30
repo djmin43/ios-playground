3dc88b5961ab6e9f2dec83f75661dd7879173163
jay <dj.min43@gmail.com>
2023-09-30 15:40:16 +0900

    view model

---
+++ memorize/memorize/emoji-momory-game.swift
@@ -0,0 +1,18 @@
+//
+//  emoji-momory-game.swift
+//  memorize
+//
+//  Created by dongjoon min on 2023/09/30.
+//
+
+
+//** ViewModel is part of UI!
+//It will 'know' about UI.
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String>
+    
+}
+

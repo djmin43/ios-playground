9f0113ab56ea084fa9a44f822b8b0e624b6bfccc
jay <dj.min43@gmail.com>
2023-10-03 14:26:11 +0900

    add clicker

--- baseball-clicker/baseball-clicker Watch App/ContentView.swift
+++ baseball-clicker/baseball-clicker Watch App/ContentView.swift
@@ -1,24 +1,47 @@
-//
-//  ContentView.swift
-//  baseball-clicker Watch App
-//
-//  Created by dongjoon min on 2023/10/03.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel: BaseballClicker
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            LazyVGrid(columns: [GridItem(), GridItem()]) {
+                CircleButton(color: .blue, action: viewModel.addStrike, text: "strike")
+                CircleButton(color: .green, action: viewModel.addBall, text: "ball")
+                CircleButton(color: .red, action: viewModel.addOut, text: "out")
+                ZStack {
+                    Circle()
+                        .strokeBorder(.white, lineWidth: 2)
+                        .fill(.clear)
+                    Text("\(viewModel.ball)  - \(viewModel.strike)\n\(viewModel.out) out")
+                }
+            }
+            
         }
-        .padding()
+        .navigationTitle("Baseball Clicker")
     }
 }
 
+struct CircleButton: View {
+    var color: Color
+    var action: () -> Void
+    var text: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .strokeBorder(color, lineWidth: 2)
+                .fill(.clear)
+            Text(text)
+        }
+        .onTapGesture(count: 1) {
+            action()
+        }
+    }
+}
+
+
 #Preview {
-    ContentView()
+    ContentView(viewModel: BaseballClicker())
 }
--- baseball-clicker/baseball-clicker Watch App/baseball_clickerApp.swift
+++ baseball-clicker/baseball-clicker Watch App/baseball_clickerApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct baseball_clicker_Watch_AppApp: App {
+    @StateObject var game = BaseballClicker()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: game)
         }
     }
 }

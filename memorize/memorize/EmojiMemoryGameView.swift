3fabbf7b39a1ef82c6899b8540b915d98517ab96
jay <dj.min43@gmail.com>
2023-11-05 09:54:11 +0900

    lecutre 9 wip

--- memorize/memorize/EmojiMemoryGameView.swift
+++ memorize/memorize/EmojiMemoryGameView.swift
@@ -50,21 +50,27 @@ struct EmojiMemoryGameView: View {
             CardView(card)
                 .padding(spacing)
                 .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
+                .zIndex(scoreChange(causedBy: card) != 0 ? 1: 0)
                 .onTapGesture {
-                    withAnimation(.easeInOut(duration: 1)) {
-                        let scoreBeforeChoosing = viewModel.score
-                        viewModel.choose(card)
-                        let scoreChange = viewModel.score - scoreBeforeChoosing
-                        lastScoreChange = (scoreChange, causedByCardId: card.id)
-                    }
+                    choose(card)
                 }
+        }
+    }
+    
+    private func choose(_ card: Card) {
+        withAnimation(.easeInOut(duration: 1)) {
+            let scoreBeforeChoosing = viewModel.score
+            viewModel.choose(card)
+            let scoreChange = viewModel.score - scoreBeforeChoosing
+            lastScoreChange = (scoreChange, causedByCardId: card.id)
         }
     }
     
     @State private var lastScoreChange = (0, causedByCardId: "")
     
-    private func scoreChange(causedBy: Card) -> Int {
-        return 0
+    private func scoreChange(causedBy card: Card) -> Int {
+        let (amount, id) = lastScoreChange
+        return card.id == id ? amount : 0
     }
     
 }
